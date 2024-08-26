cad020d17341bbbf37313f3dcc763ef9cd5b05db
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-08-26 13:55:27 +0530

    Create  when as statement.kt

---
+++ when as statement.kt
@@ -0,0 +1,9 @@
+fun main() {
+    // when as a statemnt
+    val num = 3
+    when{
+        num%2==0 -> println("$num is even")
+        num%2!=0 -> println("$num is odd")
+        //else -> println("Unexpected Number")
+    }
+}
