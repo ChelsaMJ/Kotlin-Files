c2843bdda58759cd65c073a6b5cfcca103ac2e1f
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-08-26 13:24:36 +0530

    Create if-else 2.kt

---
+++ if-else 2.kt
@@ -0,0 +1,22 @@
+fun main() {
+    
+    //if-else statement
+    val grade = 46
+    if(grade>=90)
+    {
+        println("Grade: a")
+    }
+    else if(grade>=85)
+    {
+        println("Grade: B")
+    }
+    else if(grade>=70)
+    {
+        println("Grade: C")
+    }
+    else
+    {
+        println("Grade: D")
+    }
+    
+}
