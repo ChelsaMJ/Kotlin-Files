2d765524e0d2fd6a4861700d48f4abb70dea630d
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-08-26 13:39:31 +0530

    Create if-else 4.kt

---
+++ if-else 4.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+
+    val age=15
+    if(age<13)
+    {
+        println("Child")
+    }
+    else if(age>=13 && age<=19) //using and
+    {
+        println("Teenager")
+    }
+    else
+    {
+        println("Adult")
+    }
+    
+}
