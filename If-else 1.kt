3d4cf6495014a6751e8725f66cb0816f11c38ec9
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-08-26 13:21:24 +0530

    Create If-else 1.kt

---
+++ If-else 1.kt
@@ -0,0 +1,23 @@
+fun main() {
+    //if statement
+    val temp = 40
+    if(temp>25){
+        println("Its hot in here~")
+    }
+    
+    println('\n')
+    //if-else statement
+    val age=20
+    if(age==18)
+    {
+        println("Turned adult")
+    }
+    else if(age>=18){
+        println("Been an adult")
+    }
+    else
+    {
+        println("Still a minor")
+    }
+    
+}
