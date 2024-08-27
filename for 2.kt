749aab5fbcf914a60c0e26051942d07e41d894a4
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-08-27 13:24:35 +0530

    Create for 2.kt

---
+++ for 2.kt
@@ -0,0 +1,14 @@
+fun main() {
+ 
+    var i:Int
+    //reverse
+    for(i in 5 downTo 1){
+        println(i)
+    }
+    
+    println('\n')
+    //reverse with step size
+    for(i in 5 downTo 1 step 2){
+        println(i)
+    }
+}
