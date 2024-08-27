99325ac50350bfd80904007ea5d858015ebbc64d
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-08-27 13:42:17 +0530

    Create do-while 1.kt

---
+++ do-while 1.kt
@@ -0,0 +1,15 @@
+fun main() {
+ 
+    var i=5
+    do{
+        println(i)
+        i--
+    } while(i>5)
+    //do runs first
+    //prints 5
+    //then 5--
+    //goes to while
+    //4>5?? NO
+    //loop stops
+    //only prints 5
+}
