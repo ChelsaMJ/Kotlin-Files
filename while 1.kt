3e415d896273e7c593afc9ebf89db2f4cc0798f0
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-08-27 13:39:13 +0530

    Create while 1.kt

---
+++ while 1.kt
@@ -0,0 +1,18 @@
+fun main() {
+ 
+    var i=5
+    while(i>0)
+    {
+        println("$i")
+        i--
+    }
+    
+    println("\n")
+    var j=5
+    while(j>0)
+    {
+        println("$j")
+        --j
+    }
+    
+}
