6a8013acbc7529bca5af869ae332f81a0602a6f7
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-08-28 13:57:02 +0530

    Create class 1.kt

---
+++ class 1.kt
@@ -0,0 +1,9 @@
+fun main(){
+    var s1=Student() //student is object
+    s1.result() //s1 is pointer
+}
+class Student{ //class
+    fun result(){ //function
+        println("Pending torture")
+    }
+}
