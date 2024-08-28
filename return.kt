f126657ebbc2d11b69b02d3521dd9e3ba80a2b76
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-08-28 13:21:58 +0530

    Create return.kt

---
+++ return.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    val a=10
+    val b=20
+    val result=sum(a,b) //passed a and b to function
+    println(result)
+}
+
+fun sum(a:Int,b:Int):Int{
+    val x = a+b
+    return x
+}
