98d0ef8d6803284194abaff265f2fd636417b1b0
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-08-28 13:35:48 +0530

    Update Temp1.kt

--- Temp1.kt
+++ Temp1.kt
@@ -1,14 +1,14 @@
 fun cnvrt(temp:Double):Double {
     //function to convert temperature from celcius to fahrenheit
 //     °F = °C × (9/5) + 32.
-    var t = temp*(9/5.0)+32
+    val t = temp*(9/5.0)+32
     return t
 }
 
 fun main(args: Array<String>){
     
-    var temp=25.0
-    var f:Double =cnvrt(temp)
+    val temp=25.0
+    val f:Double =cnvrt(temp)
     println(f)
     
 }
