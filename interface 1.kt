782f848efafde516f725f142d3a5e2df7f650a26
Chelsa MJ <113827875+ChelsaMJ@users.noreply.github.com>
2024-09-03 13:17:37 +0530

    Create interface 1.kt

---
+++ interface 1.kt
@@ -0,0 +1,30 @@
+interface shape{
+    
+    fun area(): Double
+}
+
+open class Rectangle(val a: Double, val b: Double):shape {
+    
+    override fun area(): Double {
+        return a * b
+    }
+    
+    open fun display() {
+        println("Area of rectangle with dimensions $a * $b is ${area()}")
+    }
+}
+
+class Square(side: Double) : Rectangle(side, side) {
+    
+    override fun display() {
+        println("Area of square with side $a is ${area()}")
+    }
+}
+
+fun main() {
+    val myRec = Rectangle(4.0, 5.0)
+    myRec.display()
+    
+    val mySq = Square(3.0)
+    mySq.display()
+}
